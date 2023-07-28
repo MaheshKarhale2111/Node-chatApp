192b83df75c01e486b532b818a4e0b456489e428
MaheshKarhale2111 <maheshkarhale03@gmail.com>
2023-07-28 21:56:01 +0530

    changes in css

--- js/client.js
+++ js/client.js
@@ -3,9 +3,18 @@ const form  = document.getElementById('form-class')
 const messageInput = document.getElementById('sendText')
 const messageContainer = document.querySelector('.container')
 
-var sendAudio = new Audio('sounds/sendSound.mp3')
+// var sendAudio = new Audio('sounds/sendSound.mp3')
 var receiveAudio = new Audio('sounds/receiveSound.mp3')
 
+
+// use enter key to submit text 
+// document.getElementById("sendText").addEventListener("keyup", function(event) {
+//     if (event.key === "Enter") {
+//         console.log("Enter key pressed");
+//         // Call the submitForm function to submit the form
+//         submitForm();
+//     }
+// });
 
 const append = (message,position)=>{
     const messageElement = document.createElement('div');
@@ -18,20 +27,28 @@ const append = (message,position)=>{
     }
 }
 
+
 form.addEventListener('submit',(e)=>{
     e.preventDefault(); // to stop reload 
     // More often than not, you don't want to submit the form to the server but you often want to stop the form from submitting, grab those details with JS and continue.
     const message = messageInput.value; 
-    append(`You : ${message}`,'right');
-    socket.emit('send',message); 
-    messageInput.value = '' ; // empty the text area 
-    sendAudio.play();  
+    if(message){
+        append(`You : ${message}`,'right');
+        socket.emit('send',message); 
+        messageInput.value = '' ; // empty the text area 
+    }
+    else {
+        alert(`${yourName} You can't send empty message`);
+    }
+    
 })
 
 
 
+
+
 const yourName = prompt("Enter your name to join")
-console.log(yourName); 
+// console.log(yourName); 
 socket.emit('new-user-joined', yourName);
 // this will trigger *socket.on('new-user-joined',(name)=>{* function from index.js 
 // and pass name parameter 
